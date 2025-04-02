49b777428df0d6dbb1183c8cad00f830be1828ac
MichalRymarski <michal.rymarski2003@gmail.com>
2025-04-02 17:54:47 +0200

    fix compose and setup application properties

---
+++ src/main/java/com/team/backend/test/User.java
@@ -0,0 +1,18 @@
+package com.team.backend.test;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String name;
+    private String email;
+}
---
+++ src/main/java/com/team/backend/test/UserRepository.java
@@ -0,0 +1,6 @@
+package com.team.backend.test;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
