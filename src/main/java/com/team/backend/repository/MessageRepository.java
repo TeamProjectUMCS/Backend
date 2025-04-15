4df7e8022a126f85a79b9a355e06ab14cb443ff5
Kacper Dęga <kacperdgf@o2.pl>
2025-04-15 18:09:00 +0200

    SCRUM-72 repositories 2.0

--- src/main/java/com/team/backend/repository/MessageRepository.java
+++ src/main/java/com/team/backend/repository/MessageRepository.java
@@ -2,9 +2,11 @@ package com.team.backend.repository;
 
 import com.team.backend.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
     Message save(Message message);
 
--- src/main/java/com/team/backend/repository/UserRepository.java
+++ src/main/java/com/team/backend/repository/UserRepository.java
@@ -2,8 +2,11 @@ package com.team.backend.repository;
 
 import com.team.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findById(Long id);
 
