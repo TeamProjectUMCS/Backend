e8295fe6b4f562ceef8abcd98d5ef1e2f6ed1026
MichalRymarski <michal.rymarski2003@gmail.com>
2025-04-20 09:58:38 +0200

    Check if test workflow works

--- src/test/java/com/team/backend/controller/LoginAndRegisterRestControllerTest.java
+++ src/test/java/com/team/backend/controller/LoginAndRegisterRestControllerTest.java
@@ -9,14 +9,12 @@ import com.team.backend.model.dto.LoginResponseDto;
 import com.team.backend.model.dto.RegisterRequest;
 import com.team.backend.model.dto.RegisterResponseDto;
 import com.team.backend.model.mapper.LoginAndRegisterMapper;
+import com.team.backend.model.mapper.UserMapper;
 import com.team.backend.service.LoginAndRegisterService;
 import com.team.backend.service.PasswordEncoderService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
@@ -24,10 +22,9 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(LoginAndRegisterRestController.class)
@@ -35,6 +32,9 @@ class LoginAndRegisterRestControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockitoBean
+    private UserMapper userMapper;
 
     @MockitoBean
     private LoginAndRegisterService loginAndRegisterService;
